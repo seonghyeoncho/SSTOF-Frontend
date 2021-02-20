a3f89af534f153aba41963f0a40a65edcb92641a
sangwoo-98 <robbyra@gmail.com>
2021-02-20 18:16:40 +0900

    Initial settings

---
+++ src/components/main/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Main: React.FC = () => (
+  <>
+    <h1>SSUTackOverflow 메인 페이지입니다.</h1>
+    <h2>화이팅~~</h2>
+  </>
+);
+
+export default Main;
---
+++ src/routes/main/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import MainComponent from "../../components/main";
+
+const Main: React.FC = () => <MainComponent />;
+
+export default Main;
