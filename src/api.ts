eb528de9c3ab9e48bd669aa34bcdfb39f435396e
sangwoo-98 <robbyra@gmail.com>
2021-02-23 13:08:14 +0900

    Setup API environment

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
+import { IUserCreateRequest } from "./components/user/interfaces/user-create-request.interface";
 
 const api = axios.create({
   baseURL: "http://52.79.87.105:8080/v1/",
 });
 
-export const userApi = {};
+export const userApi = {
+  signUp: (body: IUserCreateRequest) => api.post("user", body),
+};
