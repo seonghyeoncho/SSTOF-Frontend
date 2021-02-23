82917c47c5d42242cbd6a2ddba5dbdc71af98909
sangwoo-98 <robbyra@gmail.com>
2021-02-23 13:19:32 +0900

    Add antd to dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./components/App";
 import theme from "./theme";
+import "antd/dist/antd.css";
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
