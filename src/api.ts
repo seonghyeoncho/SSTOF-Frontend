c9ac294fa1c001959eb442857b9bb3962062238c
sangwoo-98 <robbyra@gmail.com>
2021-02-23 15:48:07 +0900

    Hide URL

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { IUserCreateRequest } from "./components/user/interfaces/user-create-request.interface";
 
 const api = axios.create({
-  baseURL: "http://52.79.87.105:8080/v1/",
+  baseURL: process.env.REACT_APP_API_ADDRESS,
 });
 
 export const userApi = {
