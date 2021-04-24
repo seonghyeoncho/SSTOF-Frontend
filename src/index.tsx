882f74020b4029d37ad72732e78dc288ce2aa9e0
sangwoo-98 <robbyra@gmail.com>
2021-04-25 01:58:13 +0900

    index.tsx에 Redux data store 적용

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,14 @@ import { ThemeProvider } from "styled-components";
 import App from "./components/App";
 import theme from "./theme";
 import "antd/dist/antd.css";
+import { Provider } from "react-redux";
+import { Store } from "./root.redux";
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>,
-  document.getElementById("root")
+  <Provider store={Store}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById("root"),
 );
