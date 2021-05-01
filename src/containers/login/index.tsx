d9f72e046a37c7a3c199298a8e2c759915b7708f
sangwoo-98 <robbyra@gmail.com>
2021-05-02 01:09:28 +0900

    Redux Root Data Store에 login 등록

---
+++ src/containers/login/index.tsx
@@ -0,0 +1,6 @@
+import LoginContainer from "./login";
+import loginPageSaga from "./saga";
+import login_reducer from "./slice";
+
+export default LoginContainer;
+export { loginPageSaga, login_reducer };
